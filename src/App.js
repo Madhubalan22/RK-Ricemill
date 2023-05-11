aee6d90b1d5d9dbcfd53639c63519c902d3fd400
Madhubalan22 <mmadhubalan14@gmail.com>
2023-05-11 11:01:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Layout from './components/layout';
+import Home from './pages/home';
+import About from "./pages/about";
+import Contact from './pages/contact';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+    <Home />
+    <Routes>
+     
+    <Route path='/about' component={About} />
+    </Routes>
+    </>
+  )
+}
+
+export default App;
